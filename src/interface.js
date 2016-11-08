84abfbf42d47acb7dec95a1ce53f8f5c59d8d7e0
Dionysis <Dionysis.k22@gmail.com>
2016-11-08 23:46:21 +0000

    abbr links at each creation of note

--- src/interface.js
+++ src/interface.js
@@ -7,8 +7,17 @@ function saveNote() {
   noteApp.createNewNote(arrayNote);
   noteApp.abbrevNote(arrayNote);
   document.noteForm.reset();
+  addLinkNotes();
 }
 
 function addLinkNotes() {
-  var formNote = doxu
-}
+  link = ""
+  for (i=0; i<noteApp.notes.length; i++) {
+    myDiv = document.getElementById('linkNotes');
+    // var aTag = document.createElement('a');
+    // aTag.setAttribute("href","#" + noteApp.sliced[i]);
+    // aTag.inner.HTML = noteApp.sliced[i];
+    link += "<a href='#"+noteApp.notes[i]+"'>"+noteApp.sliced[i]+"</a><br>"
+    myDiv.innerHTML = link;
+  };
+};
