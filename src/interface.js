a34dd9e34b742692900365fc902fb842f3b8be38
Anna Lucking <annavlucking@hotmail.co.uk>
2016-11-09 12:26:52 +0000

    update displayAbbrevNotes

--- src/interface.js
+++ src/interface.js
@@ -7,33 +7,16 @@ function saveNote() {
   noteApp.createNewNote(note);
   noteApp.abbrev(note);
   document.noteForm.reset();
+  displayAbbrevNotes();
 }
+
+
 
 function displayAbbrevNotes() {
-  for (i=0;i<note.sliced.length;i++) {
-    var mydiv = document.getElementById("abbrevNotes");
-    var aTag = document.createElement('a');
-    aTag.setAttribute("href", "#" + note.sliced[i]);
-    aTag.innerHTML = note.sliced[i];
-    console.log(aTag);
-    mydiv.appendChild(aTag);
+  link = "";
+  for (i=0; i<noteApp.sliced.length; i++) {
+    myDiv = document.getElementById('abbrevNotes');
+    link += "<a href=#"+noteApp.notes[i]+">"+noteApp.sliced[i]+"</a><br>";
+    myDiv.innerHTML = link;
   }
-}
-
-function makeUrlChangeNoteForCurrentPage() {
-  window.addEventListener("hashchange", showNoteForCurrentPage);
-}
-
-function showNoteForCurrentPage() {
-  showNote(getNoteFromUrl(window.location));
-}
-
-function getNoteFromUrl(location) {
-  return location.hash.split("#")[1];
-}
-
-function showNote(note) {
-  document
-    .getElementById("abbrevNotes")
-    .innerHTML = note;
 }
