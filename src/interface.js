eef3243c49bd82455168dc60c5de10caafd307fd
Dionysis <Dionysis.k22@gmail.com>
2016-11-09 00:01:15 +0000

    full note shown on click

--- src/interface.js
+++ src/interface.js
@@ -2,6 +2,8 @@ var newNoteApp = function() {
   noteApp = new NoteApp();
 };
 
+displayFullNoteForEachLink();
+
 function saveNote() {
   arrayNote = document.getElementById('create-note-text').value;
   noteApp.createNewNote(arrayNote);
@@ -20,4 +22,20 @@ function addLinkNotes() {
     link += "<a href='#"+noteApp.notes[i]+"'>"+noteApp.sliced[i]+"</a><br>"
     myDiv.innerHTML = link;
   };
+};
+
+function displayFullNoteForEachLink() {
+  window.addEventListener("hashchange", displayFullNote)
+};
+
+function displayFullNote() {
+  displayNote(grabFromAddressBar(window.location));
+};
+
+function grabFromAddressBar(url) {
+  return url.hash.split("#")[1]
+};
+
+function displayNote(hashLinkIsNote) {
+  document.getElementById("fullNote").innerHTML = hashLinkIsNote;
 };
